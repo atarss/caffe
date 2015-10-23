70422040dfbe60cf46d4cb772f9e6c74b4ab0fac
Andy <andy9999678@gmail.com>
2015-10-23 09:49:59 +0000

    modified:   include/caffe/layer_factory.hpp

--- include/caffe/layer_factory.hpp
+++ include/caffe/layer_factory.hpp
@@ -71,7 +71,7 @@ class LayerRegistry {
 
   // Get a layer using a LayerParameter.
   static shared_ptr<Layer<Dtype> > CreateLayer(const LayerParameter& param) {
-    LOG(INFO) << "Creating layer " << param.name();
+    // LOG(INFO) << "Creating layer " << param.name();
     const string& type = param.type();
     CreatorRegistry& registry = Registry();
     CHECK_EQ(registry.count(type), 1) << "Unknown layer type: " << type
